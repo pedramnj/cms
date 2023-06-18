859e19ae7eb5d7d60f62898fe24138073e239d99
Pedram Nikjooy s317086 <s317086@studenti.polito.it>
2023-06-18 16:13:55 +0200

    app.js and index.js  and index.html

--- client/src/index.js
+++ client/src/index.js
@@ -1,1 +1,15 @@
 //Entry point for rendering the React application into the DOM
+
+/*
+index.js file is responsible for rendering the main App component into the root HTML element of the application 
+(<div id="root"></div>).
+The ReactDOM.render function is used to mount the App component into the DOM, 
+ensuring that it's rendered within the specified root element.
+*/
+// index.js
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
+
+ReactDOM.render(<App />, document.getElementById('root'));
